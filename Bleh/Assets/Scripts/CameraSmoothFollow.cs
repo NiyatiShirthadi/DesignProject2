d51200d338e08bb952db63b9f76935c562c6ad4b
NiyatiShirthadi <44228758+NiyatiShirthadi@users.noreply.github.com>
2019-09-24 15:49:26 +0530

    1st Commit

---
+++ Bleh/Assets/Scripts/CameraSmoothFollow.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using CC2D;
+
+
+public class CameraSmoothFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothDampTime = 0.2f;
+    [HideInInspector]
+    public new Transform transform;
+    public Vector3 cameraOffset;
+    public bool useFixedUpdate = false;
+
+    private Meepy_CharacterController _playerController;
+    private Vector3 _smoothDampVelocity;
+
+
+    void Awake()
+    {
+        transform = gameObject.transform;
+        _playerController = target.GetComponent<Meepy_CharacterController>();
+    }
+
+
+    void LateUpdate()
+    {
+        if (!useFixedUpdate)
+            updateCameraPosition();
+    }
+
+
+    void FixedUpdate()
+    {
+        if (useFixedUpdate)
+            updateCameraPosition();
+    }
+
+
+    void updateCameraPosition()
+    {
+        if (_playerController == null)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, target.position - cameraOffset, ref _smoothDampVelocity, smoothDampTime);
+            return;
+        }
+
+        if (_playerController.velocity.x > 0)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, target.position - cameraOffset, ref _smoothDampVelocity, smoothDampTime);
+        }
+        else
+        {
+            var leftOffset = cameraOffset;
+            leftOffset.x *= -1;
+            transform.position = Vector3.SmoothDamp(transform.position, target.position - leftOffset, ref _smoothDampVelocity, smoothDampTime);
+        }
+    }
+
+}
