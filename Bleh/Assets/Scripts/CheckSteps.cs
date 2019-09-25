f024d147abbcbe81e0cfce1e706d75b949bde3c8
NiyatiShirthadi <44228758+NiyatiShirthadi@users.noreply.github.com>
2019-09-25 12:04:16 +0530

    Added Tip tap steps in the blackness

---
+++ Bleh/Assets/Scripts/CheckSteps.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CC2D;
+
+[System.Serializable]
+public class CheckSteps : MonoBehaviour
+{
+    public GameObject Step;
+    private Meepy_CharacterController _controller;
+    //private bool SteppedOn = false;
+    public Vector3 LowerIt;
+
+    public bool Aboolyy;
+
+    /*[SerializeField]
+    public bool StepIsOn {
+        get
+        {
+            return SteppedOn;
+        }
+        set
+        {
+            SteppedOn = value;
+            if(value == true)
+            {
+                Instantiate(Step, transform.position, Quaternion.identity);
+            }
+        }
+    }*/
+   /* IEnumerator SelfDestruct()
+    {
+        yield return new WaitForSeconds(2f);
+        if (Step != null)
+        {
+            Destroy(Step);
+        }
+    }*/
+
+    private void Awake()
+    {
+        _controller = GetComponent<Meepy_CharacterController>();
+    }
+    private void Update()
+    {
+        if(Aboolyy == true && _controller.isGrounded)
+        {
+            Instantiate(Step, transform.position - LowerIt, Quaternion.identity);
+           // StartCoroutine(SelfDestruct());
+            Aboolyy = false;
+        }
+    }
+}
---
+++ Bleh/Assets/Scripts/DestroyStep.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyStep : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SelfDestruct());
+    }
+
+    IEnumerator SelfDestruct()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
+
+
+    }
+}
