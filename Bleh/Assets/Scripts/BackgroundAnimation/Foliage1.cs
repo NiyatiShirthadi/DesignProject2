bda5ae3e393a1387ec2b28addaf5c0e0ac6a48dd
NiyatiShirthadi <44228758+NiyatiShirthadi@users.noreply.github.com>
2019-10-21 21:32:37 +0530

    Foliage animations + New highlights

---
+++ Bleh/Assets/Scripts/BackgroundAnimation/Foliage1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Foliage1 : MonoBehaviour
+{
+    private Animator Anim;
+
+    private void Start()
+    {
+        Anim = gameObject.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Anim.SetBool("Ruffle", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Anim.SetBool("Ruffle", false);
+        }
+    }
+
+
+
+}
