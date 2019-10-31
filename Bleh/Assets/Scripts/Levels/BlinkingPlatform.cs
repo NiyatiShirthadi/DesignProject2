8793e9b8e5e62b8a758b5d045144c102e4154047
NiyatiShirthadi <44228758+NiyatiShirthadi@users.noreply.github.com>
2019-11-01 00:56:18 +0530

    new level8

---
+++ Bleh/Assets/Scripts/Levels/BlinkingPlatform.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkingPlatform : MonoBehaviour
+{
+    private Renderer Rend;
+    private Collider2D Col;
+
+    public float VisibleWait = 2f;
+    public float InvisibleWait = 4f;
+
+
+
+
+    void Start()
+    {
+        Rend = gameObject.GetComponent<Renderer>();
+        Col = gameObject.GetComponent<Collider2D>();
+    }
+
+    IEnumerator Visibile()
+    {
+        yield return new WaitForSeconds(VisibleWait);
+        Rend.enabled = false;
+        Col.enabled = false;
+    }
+
+    IEnumerator Invisible()
+    {
+        yield return new WaitForSeconds(InvisibleWait);
+        Rend.enabled = true;
+        Col.enabled = true;
+    }
+
+    private void Update()
+    {
+        StartCoroutine(Visibile());
+        StartCoroutine(Invisible());
+    }
+
+}
